51ad7777330c54a7a1b9a749791911fd8cd636cf
bl4ckscor3 <bl4ckscor33@gmail.com>
2020-02-14 21:13:35 +0100

    update to 1.15.2

---
+++ src/main/java/com/builtbroken/bonetorch/ClientRegistrar.java
@@ -0,0 +1,20 @@
+package com.builtbroken.bonetorch;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid=BoneTorchMod.DOMAIN, bus=Bus.MOD, value=Dist.CLIENT)
+public class ClientRegistrar
+{
+	@SubscribeEvent
+	public static void registerItems(FMLClientSetupEvent event)
+	{
+		RenderTypeLookup.setRenderLayer(BoneTorchMod.blockTorch, RenderType.cutout());
+		RenderTypeLookup.setRenderLayer(BoneTorchMod.blockTorchWall, RenderType.cutout());
+	}
+}
