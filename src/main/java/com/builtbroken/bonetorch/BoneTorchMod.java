f956c05ceb1100a8aa0e45077e0cc10b5fcb7b57
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-08-07 12:57:23 +0200

    add torch bandolier compatibility

--- src/main/java/com/builtbroken/bonetorch/BoneTorchMod.java
+++ src/main/java/com/builtbroken/bonetorch/BoneTorchMod.java
@@ -1,22 +1,24 @@
 package com.builtbroken.bonetorch;
 
+import com.builtbroken.bonetorch.compat.torchbandolier.TorchBandolierCompat;
+
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.StandingAndWallBlockItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.TorchBlock;
 import net.minecraft.world.level.block.WallTorchBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.StandingAndWallBlockItem;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ObjectHolder;
-
-import net.minecraft.world.level.block.state.BlockBehaviour;
 
 /**
  * Simple mod to add a bone torch to MC. Idea was spawned from running out of
@@ -28,24 +30,27 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 @Mod.EventBusSubscriber(bus=Bus.MOD)
 public class BoneTorchMod
 {
-    public static final String DOMAIN = "bonetorch";
-    public static final String PREFIX = DOMAIN + ":";
+	public static final String DOMAIN = "bonetorch";
+	public static final String PREFIX = DOMAIN + ":";
 
-    @ObjectHolder(PREFIX + "bonetorch")
-    public static TorchBlock blockTorch;
-    @ObjectHolder(PREFIX + "wall_bonetorch")
-    public static WallTorchBlock blockTorchWall;
+	@ObjectHolder(PREFIX + "bonetorch")
+	public static TorchBlock blockTorch;
+	@ObjectHolder(PREFIX + "wall_bonetorch")
+	public static WallTorchBlock blockTorchWall;
 
-    @SubscribeEvent
-    public static void registerItems(RegistryEvent.Register<Item> event)
-    {
-        event.getRegistry().register(new StandingAndWallBlockItem(blockTorch, blockTorchWall, new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)).setRegistryName(blockTorch.getRegistryName()));
-    }
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event)
+	{
+		event.getRegistry().register(new StandingAndWallBlockItem(blockTorch, blockTorchWall, new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)).setRegistryName(blockTorch.getRegistryName()));
 
-    @SubscribeEvent
-    public static void registerBlocks(RegistryEvent.Register<Block> event)
-    {
-        event.getRegistry().register(new TorchBlock(BlockBehaviour.Properties.of(Material.DECORATION).noCollission().strength(0.0F).lightLevel(state -> 14).sound(SoundType.WOOD), ParticleTypes.FLAME).setRegistryName(new ResourceLocation(DOMAIN, "bonetorch")));
-        event.getRegistry().register(new WallTorchBlock(BlockBehaviour.Properties.of(Material.DECORATION).noCollission().strength(0.0F).lightLevel(state -> 14).sound(SoundType.WOOD), ParticleTypes.FLAME).setRegistryName(new ResourceLocation(DOMAIN, "wall_bonetorch")));
-    }
+		if(ModList.get().isLoaded("torchbandolier"))
+			TorchBandolierCompat.register(event);
+	}
+
+	@SubscribeEvent
+	public static void registerBlocks(RegistryEvent.Register<Block> event)
+	{
+		event.getRegistry().register(new TorchBlock(BlockBehaviour.Properties.of(Material.DECORATION).noCollission().strength(0.0F).lightLevel(state -> 14).sound(SoundType.WOOD), ParticleTypes.FLAME).setRegistryName(new ResourceLocation(DOMAIN, "bonetorch")));
+		event.getRegistry().register(new WallTorchBlock(BlockBehaviour.Properties.of(Material.DECORATION).noCollission().strength(0.0F).lightLevel(state -> 14).sound(SoundType.WOOD), ParticleTypes.FLAME).setRegistryName(new ResourceLocation(DOMAIN, "wall_bonetorch")));
+	}
 }
