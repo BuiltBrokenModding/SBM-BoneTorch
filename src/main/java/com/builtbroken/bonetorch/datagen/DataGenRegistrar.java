66a7497b7f176f84d7be4fc823958fda2095ee3d
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-12-09 22:10:24 +0100

    Feature: Update to 1.21.4

--- src/main/java/com/builtbroken/bonetorch/datagen/DataGenRegistrar.java
+++ src/main/java/com/builtbroken/bonetorch/datagen/DataGenRegistrar.java
@@ -3,14 +3,10 @@ package com.builtbroken.bonetorch.datagen;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 
 import com.builtbroken.bonetorch.BoneTorchMod;
 
 import net.minecraft.DetectedVersion;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
 import net.minecraft.data.metadata.PackMetadataGenerator;
@@ -23,20 +19,17 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.EventBusSubscriber.Bus;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.neoforged.neoforge.data.event.GatherDataEvent.DataProviderFromOutputLookup;
 
 @EventBusSubscriber(modid = BoneTorchMod.DOMAIN, bus = Bus.MOD)
 public class DataGenRegistrar
 {
 	@SubscribeEvent
-	public static void onGatherData(GatherDataEvent event)
+	public static void onGatherData(GatherDataEvent.Client event)
 	{
-		DataGenerator generator = event.getGenerator();
-		PackOutput output = generator.getPackOutput();
-		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-		generator.addProvider(event.includeServer(), new LootTableProvider(output, Set.of(), List.of(new SubProviderEntry(BlockLootTableGenerator::new, LootContextParamSets.BLOCK)), lookupProvider));
-		generator.addProvider(event.includeServer(), new RecipeGenerator.Runner(output, lookupProvider));
-		generator.addProvider(true, new PackMetadataGenerator(output)
+		event.createProvider((DataProviderFromOutputLookup<LootTableProvider>) (output, lookupProvider) -> new LootTableProvider(output, Set.of(), List.of(new SubProviderEntry(BlockLootTableGenerator::new, LootContextParamSets.BLOCK)), lookupProvider));
+		event.createProvider(RecipeGenerator.Runner::new);
+		event.createProvider(output -> new PackMetadataGenerator(output)
 				.add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("SBM-BoneTorch resources & data"),
 						DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
 						Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE)))));
