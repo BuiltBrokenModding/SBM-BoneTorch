b033598d8dab357489f0ee6b27bf0d83e248efa1
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-12-11 00:08:15 +0100

    Feature: Update to 1.21.11

--- src/main/java/com/builtbroken/bonetorch/datagen/DataGenRegistrar.java
+++ src/main/java/com/builtbroken/bonetorch/datagen/DataGenRegistrar.java
@@ -1,18 +1,12 @@
 package com.builtbroken.bonetorch.datagen;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import com.builtbroken.bonetorch.BoneTorchMod;
 
-import net.minecraft.DetectedVersion;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.data.loot.LootTableProvider.SubProviderEntry;
-import net.minecraft.data.metadata.PackMetadataGenerator;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -27,12 +21,5 @@ public class DataGenRegistrar
 	{
 		event.createProvider((DataProviderFromOutputLookup<LootTableProvider>) (output, lookupProvider) -> new LootTableProvider(output, Set.of(), List.of(new SubProviderEntry(BlockLootTableGenerator::new, LootContextParamSets.BLOCK)), lookupProvider));
 		event.createProvider(RecipeGenerator.Runner::new);
-		event.createProvider(output -> new PackMetadataGenerator(output)
-			.add(PackMetadataSection.CLIENT_TYPE, new PackMetadataSection(
-				Component.literal("SBM-BoneTorch resources"),
-				DetectedVersion.BUILT_IN.packVersion(PackType.CLIENT_RESOURCES).minorRange()))
-			.add(PackMetadataSection.SERVER_TYPE, new PackMetadataSection(
-				Component.literal("SBM-BoneTorch data"),
-				DetectedVersion.BUILT_IN.packVersion(PackType.SERVER_DATA).minorRange())));
 	}
 }
